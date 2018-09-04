5516f271a99a5b8d3a47bbc14a39d030b6cb7183
dnk777 <dnk777@users.noreply.github.com>
2018-09-05 00:31:06 +0300

    Add utilities for operating on int16_t links

--- source/qalgo/Links.h
+++ source/qalgo/Links.h
@@ -3,8 +3,6 @@
 
 #include <assert.h>
 
-// TODO: Lift also utilities that operate on int16_t offsets instead of pointers here
-
 /**
  * Links an item that has an intrusive array of links to a list head.
  * The item becomes the list head, the former head is linked to the "next" link
@@ -59,6 +57,85 @@ static inline Item *Unlink( Item *item, Item **listHeadRef, int listIndex ) {
 
 	item->prev[listIndex] = nullptr;
 	item->next[listIndex] = nullptr;
+	return item;
+}
+
+/**
+ * A specialized version of {@code Link} that is similar to {@code Link(Item *, Item**, int)}
+ * but operates on 16-bit signed indices for links instead of pointers
+ * and accepts a base array as an additional argument.
+ * Non-negative link indices are assumed to be valid and refer to the corresponding {@code basePtr} cells.
+ * Negative link indices indicate "null" links.
+ * These utilities are intended to be used if memory consumption/cache efficiency matters
+ * ({@code basePtr} usually do not belongs to an item but is kept outside it).
+ * @tparam Item any type that has two accessible arrays of links:
+ * {@code int16_t *prev[]} and {@code int16_t *next[]}.
+ * There arrays should have a size corresponding to a desired number of list an item can be linked to simultaneously.
+ * @param item an item to link
+ * @param listHeadRef an address of the current list head
+ * @param listIndex an index of list to link to
+ * @param basePtr an address of items storage the link indices refer to
+ * @return the newly linked item (same as the argument) conforming to fluent API style.
+ */
+template<typename Item>
+static inline Item *Link( Item *item, int16_t *listHeadRef, int listIndex, Item *basePtr ) {
+	const intptr_t offset = item - basePtr;
+	assert( offset >= 0 );
+	assert( offset <= ( 1 << 15u ) );
+	const auto itemIndex = (int16_t)offset;
+
+	// If the list head is defined (is not "null"), set its prev link to the newly linked item
+	if( *listHeadRef >= 0 ) {
+		Item *headItem = basePtr + *listHeadRef;
+		headItem->prev[listIndex] = itemIndex;
+	}
+
+	item->prev[listIndex] = -1;
+	item->next[listIndex] = *listHeadRef;
+
+	*listHeadRef = itemIndex;
+
+	return item;
+}
+
+/**
+ * A specialized version of {@code Unlink} that is similar to {@code Unlink(Item *, Item**, int)}
+ * but operates on 16-bit signed indices for links instead of pointers
+ * and accepts a base array as an additional argument.
+ * Non-negative link indices are assumed to be valid and refer to the corresponding {@code basePtr} cells.
+ * Negative link indices indicate "null" links.
+ * These utilities are intended to be used if memory consumption/cache efficiency matters
+ * ({@code basePtr} usually do not belongs to an item but is kept outside it).
+ * @tparam Item any type that has two accessible arrays of links:
+ * {@code int16_t *prev[]} and {@code int16_t *next[]}.
+ * There arrays should have a size corresponding to a desired number of list an item can be linked to simultaneously.
+ * @param item an item to unlink
+ * @param listHeadRef an address of the current list head
+ * @param listIndex an index of list to unlink from
+ * @param basePtr an address of items storage the link indices refer to
+ * @return the newly unlinked item (same as the argument) conforming to fluent API style.
+ */
+template <typename Item>
+static inline Item *Unlink( Item *item, int16_t *listHeadRef, int listIndex, Item *basePtr ) {
+	const int16_t nextItemIndex = item->next[listIndex];
+	// If the next item for the item is defined
+	if( nextItemIndex >= 0 ) {
+		Item *nextItem = basePtr + nextItemIndex;
+		nextItem->prev[listIndex] = item->prev[listIndex];
+	}
+	const int16_t prevItemIndex = item->prev[listIndex];
+	// If the prev item for the item is defined
+	if( prevItemIndex >= 0 ) {
+		Item *prevItem = basePtr + prevItemIndex;
+		prevItem->next[listIndex] = item->next[listIndex];
+	} else {
+		// Check whether this item is the list head
+		assert( item - basePtr == *listHeadRef );
+		*listHeadRef = item->next[listIndex];
+	}
+
+	item->prev[listIndex] = -1;
+	item->next[listIndex] = -1;
 	return item;
 }
 
